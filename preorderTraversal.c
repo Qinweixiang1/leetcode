c713b7dbf2bd4155c6b163e08c7f0e4bbd3b3e92
Qinweixiang1 <46370870+Qinweixiang1@users.noreply.github.com>
2022-06-25 12:55:44 +0800

    Update preorderTraversal.c

--- preorderTraversal.c
+++ preorderTraversal.c
@@ -31,3 +31,57 @@ int* preorderTraversal(Tree T, int* returnSize){
 }
 
 /*非递归*/
+#define N 10
+typedef struct stack *Stack;
+typedef struct TreeNode *Tree;
+
+struct stack{
+    Tree *base;
+    int top;
+};
+
+Stack InitStack(Stack S){
+    S=(Stack)malloc(sizeof(struct stack));
+    S->base=(Tree*)malloc(sizeof(Tree)*N);
+    S->top=0;
+    return S;
+}
+
+
+int StackFull(Stack S){
+    return S->top==N;
+}
+
+int StackEmpty(Stack S){
+    return S->top==0;
+}
+
+int push(Stack S, Tree e){
+    if(StackFull(S)) return 0;
+    S->base[S->top++]=e;
+    return 1;
+}
+
+Tree pop(Stack S){
+    if(StackEmpty(S)) return NULL;
+    return S->base[--(S->top)];
+}
+
+int* preorderTraversal(Tree T, int* returnSize){
+    int *result=(int*)malloc(sizeof(int)*100); * returnSize=0;
+    Stack S;
+    S=InitStack(S);
+    Tree p=T;
+    while(p||!StackEmpty(S)){
+        if(p){
+            result[(* returnSize)++] = p->val;
+            push(S,p);
+            p=p->left;
+        }
+        else{
+            p=pop(S);
+            p=p->right;
+        }
+    }
+    return result;
+}
