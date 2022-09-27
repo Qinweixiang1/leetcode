f5db2c2ec1a45c42cb2253a07bba9aa9ba404e18
Qinweixiang1 <46370870+Qinweixiang1@users.noreply.github.com>
2022-09-27 17:22:22 +0800

    Update coinChange.c

--- coinChange.c
+++ coinChange.c
@@ -1,3 +1,24 @@
+// 不带备忘录
+int minMoney(int* arr, int arrLen, int aim ) {
+    if (aim == 0)
+        return 0;
+    int ans = INT_MAX;
+    for (int i=0; i< arrLen; i++) {
+        // 金额不可达
+        if (aim - arr[i] < 0)
+            continue;
+        int subProb = minMoney(arr,arrLen, aim-arr[i] );
+        // 子问题无解
+        if (subProb == -1)
+            continue;
+        ans = fmin(ans, subProb + 1);
+    }
+    return ans == INT_MAX ? -1 : ans;
+}
+
+/*==========================================*/
+
+// 带备忘录
 int helper(int* coins, int coinsSize, int amount, int *memo){
     if(!amount)
         return 0;
