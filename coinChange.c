40ef0ebaa2c2588e31ef3d254f9a230ea06a4f65
Qinweixiang1 <46370870+Qinweixiang1@users.noreply.github.com>
2022-09-27 16:25:25 +0800

    Create coinChange.c

---
+++ coinChange.c
@@ -0,0 +1,26 @@
+int helper(int* coins, int coinsSize, int amount, int *memo){
+    if(!amount)
+        return 0;
+    if(memo[amount]!=-2)
+        return memo[amount];
+    int ans=INT_MAX;
+    for(int i=0; i<coinsSize; i++){
+      // 金额不可达
+        if(amount - coins[i]  < 0)
+            continue;
+        int subProb = helper(coins, coinsSize, amount-coins[i], memo);
+      // 子问题无解
+        if(subProb == -1)
+            continue;
+        ans=fmin(ans, subProb+1);
+    }
+    memo[amount]= ans == INT_MAX ? -1 : ans;
+    return memo[amount];
+}
+
+int coinChange(int* coins, int coinsSize, int amount){
+int *memo = (int*)malloc(sizeof(int)* (amount + 1));
+for(int i =0; i<amount+1; i++)
+    memo[i]=-2;
+return helper(coins, coinsSize, amount, memo);
+}
