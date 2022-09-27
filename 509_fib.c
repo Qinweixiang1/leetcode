085c1ffecc4bb63eb61a3d6d534bad176c32ad93
Qinweixiang1 <46370870+Qinweixiang1@users.noreply.github.com>
2022-09-27 16:45:12 +0800

    Update 509_fib.c

--- 509_fib.c
+++ 509_fib.c
@@ -1,4 +1,12 @@
 int fib(int n){
+if( n < 2)
+    return n;
+return fib(n-1) + fib(n-2);
+}
+
+/*=========================================================*/
+
+int fib(int n){
 if(n<2)
     return n;
 int prev = 0, current = 1;
