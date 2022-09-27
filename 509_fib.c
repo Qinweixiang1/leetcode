8155a18b17eab711239e9edc940d347086321b4d
Qinweixiang1 <46370870+Qinweixiang1@users.noreply.github.com>
2022-09-27 16:59:11 +0800

    Update 509_fib.c

--- 509_fib.c
+++ 509_fib.c
@@ -1,3 +1,4 @@
+// 暴力
 int fib(int n){
 if( n < 2)
     return n;
@@ -5,7 +6,37 @@ return fib(n-1) + fib(n-2);
 }
 
 /*=========================================================*/
+// 带备忘录 自顶向下
+int helper(int n, int *memo){
+    if(n<2)
+        return n;
+    if(memo[n]!=0)
+        return memo[n];
+    memo[n] = helper(n-1, memo) + helper(n-2, memo);
+    return memo[n];
+}
 
+int fib(int n){
+int *memo=(int*)malloc(sizeof(int) * (n + 1));
+memset(memo, 0, sizeof(int) * (n + 1));
+return helper(n, memo);
+}
+
+/*=========================================================*/
+// 带备忘录 自底向上
+
+int fib(int n){
+if(n<2)
+    return n;
+int *memo=(int*)malloc(sizeof(int) * (n + 1));
+memo[0]=0; memo[1]=1;
+for(int i=2; i<=n; i++)
+    memo[i]=memo[i-1] + memo[i-2];
+return memo[n];
+}
+
+/*========================================================*/
+// 优化备忘录
 int fib(int n){
 if(n<2)
     return n;
